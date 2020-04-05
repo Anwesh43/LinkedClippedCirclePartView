525c0931a171da7bfc96886dd2af2c755bb818c9
Anwesh43 <anweshthecool0@gmail.com>
2020-04-06 04:57:15 +0530

    adding ClippedCirclePart class

--- clippedcirclepartview/src/main/java/com/anwesh/uiprojects/clippedcirclepartview/ClippedCirclePartView.kt
+++ clippedcirclepartview/src/main/java/com/anwesh/uiprojects/clippedcirclepartview/ClippedCirclePartView.kt
@@ -160,4 +160,28 @@ class ClippedCirclePartView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class ClippedCirclePart(var i : Int, val state : State = State()) {
+
+        private val root : CCPNode = CCPNode(0)
+        private var curr : CCPNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
