6bedb1a1b9d5f29250cd93219d9a86af9f7ac0ee
Anwesh43 <anweshthecool0@gmail.com>
2020-04-06 04:14:06 +0530

    adding libraries and constatns

--- clippedcirclepartview/src/main/java/com/anwesh/uiprojects/clippedcirclepartview/ClippedCirclePartView.kt
+++ clippedcirclepartview/src/main/java/com/anwesh/uiprojects/clippedcirclepartview/ClippedCirclePartView.kt
@@ -4,3 +4,20 @@ package com.anwesh.uiprojects.clippedcirclepartview
  * Created by anweshmishra on 06/04/20.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Path
+
+val nodes : Int = 5
+val parts : Int = 5
+val scGap : Float = 0.02f
+val delay : Long = 20
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#3F51B5")
+val backColor : Int = Color.parseColor("#BDBDBD")
+
