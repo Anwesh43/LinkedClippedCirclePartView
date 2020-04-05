cfe5060b733c08ae7242a5d677590cb9d5b09366
Anwesh43 <anweshthecool0@gmail.com>
2020-04-06 05:09:31 +0530

    adding static create method

--- clippedcirclepartview/src/main/java/com/anwesh/uiprojects/clippedcirclepartview/ClippedCirclePartView.kt
+++ clippedcirclepartview/src/main/java/com/anwesh/uiprojects/clippedcirclepartview/ClippedCirclePartView.kt
@@ -209,4 +209,13 @@ class ClippedCirclePartView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : ClippedCirclePartView {
+            val view : ClippedCirclePartView = ClippedCirclePartView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
